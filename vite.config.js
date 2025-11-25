52a6caaab4f3f66666254dac09faaf1484ab26b1
ChiChiBeek <me@thijs.com>
2025-11-25 21:00:30 +0100

    Update inverted logo

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icon-192x192-v2.png', // put these in `public/`
+            src: '/icon-192x192-v3.png', // put these in `public/`
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512-v2.png',
+            src: '/icon-512x512-v3.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/maskable-icon-192x192-v2.png',
+            src: '/maskable-icon-192x192-v3.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
